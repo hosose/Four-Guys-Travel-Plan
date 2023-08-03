70a6897a0b40859d14fa89e21912d222ccc08973
USER <USER@kosta17>
2023-08-03 14:17:49 +0900

    First Commit

---
+++ FourGuys/src/main/java/org/kosta/fourguys/FourGuysApplication.java
@@ -0,0 +1,13 @@
+package org.kosta.fourguys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourGuysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourGuysApplication.class, args);
+	}
+
+}
---
+++ FourGuys/src/test/java/org/kosta/fourguys/FourGuysApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kosta.fourguys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourGuysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
