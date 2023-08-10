efcde0d8fd5fdadaa47ad1f0fb827f4ec68c2181
USER <USER@kosta17>
2023-08-10 15:22:39 +0900

    fix kakao

--- FourGuys/src/main/resources/static/planner.clx.js
+++ FourGuys/src/main/resources/static/planner.clx.js
@@ -60,7 +60,109 @@
 			// End - User Script
 			
 			// Header
-			app.supportMedia("all", "default");
+			var dataSet_1 = new cpr.data.DataSet("jeju");
+			dataSet_1.parseData({
+				"columns" : [
+					{
+						"name": "contentid",
+						"dataType": "string"
+					},
+					{
+						"name": "mapx",
+						"dataType": "string"
+					},
+					{
+						"name": "mapy",
+						"dataType": "string"
+					},
+					{
+						"name": "title",
+						"dataType": "string"
+					},
+					{
+						"name": "tel",
+						"dataType": "string"
+					},
+					{
+						"name": "firstimage",
+						"dataType": "string"
+					},
+					{
+						"name": "addr2",
+						"dataType": "string"
+					},
+					{
+						"name": "addr1",
+						"dataType": "string"
+					},
+					{
+						"name": "cat1",
+						"dataType": "string"
+					},
+					{
+						"name": "cat2",
+						"dataType": "string"
+					},
+					{
+						"name": "cat3",
+						"dataType": "string"
+					}
+				]
+			});
+			app.register(dataSet_1);
+			
+			var dataSet_2 = new cpr.data.DataSet("planDate");
+			dataSet_2.parseData({
+				"columns" : [
+					{
+						"name": "planDate",
+						"dataType": "string"
+					},
+					{
+						"name": "plannerNo",
+						"dataType": "string"
+					}
+				]
+			});
+			app.register(dataSet_2);
+			var dataMap_1 = new cpr.data.DataMap("areaSearch");
+			dataMap_1.parseData({
+				"columns" : [{"name": "title"}]
+			});
+			app.register(dataMap_1);
+			
+			var dataMap_2 = new cpr.data.DataMap("plannerNoDM");
+			dataMap_2.parseData({
+				"columns" : [{"name": "plannerNo"}]
+			});
+			app.register(dataMap_2);
+			var submission_1 = new cpr.protocols.Submission("subSave");
+			app.register(submission_1);
+			
+			var submission_2 = new cpr.protocols.Submission("areaList");
+			submission_2.method = "get";
+			submission_2.action = "/findAllArea";
+			submission_2.addRequestData(dataMap_1);
+			submission_2.addResponseData(dataSet_1, false);
+			if(typeof onAreaListSubmitSuccess == "function") {
+				submission_2.addEventListener("submit-success", onAreaListSubmitSuccess);
+			}
+			app.register(submission_2);
+			
+			var submission_3 = new cpr.protocols.Submission("loginCheck");
+			submission_3.method = "get";
+			submission_3.action = "loginCheck";
+			app.register(submission_3);
+			
+			var submission_4 = new cpr.protocols.Submission("dayBtnSM");
+			submission_4.method = "get";
+			submission_4.action = "getDay";
+			submission_4.addRequestData(dataMap_2);
+			submission_4.addResponseData(dataSet_2, false);
+			app.register(submission_4);
+			app.supportMedia("all and (min-width: 1024px)", "default");
+			app.supportMedia("all and (min-width: 500px) and (max-width: 1023px)", "tablet");
+			app.supportMedia("all and (max-width: 499px)", "mobile");
 			
 			// Configure root container
 			var container = app.getContainer();
@@ -70,8 +172,264 @@
 			});
 			
 			// Layout
+			var xYLayout_1 = new cpr.controls.layouts.XYLayout();
+			container.setLayout(xYLayout_1);
 			
 			// UI Configuration
+			var group_1 = new cpr.controls.Container();
+			var xYLayout_2 = new cpr.controls.layouts.XYLayout();
+			group_1.setLayout(xYLayout_2);
+			(function(container){
+				var textArea_1 = new cpr.controls.TextArea("txa2");
+				textArea_1.value = "플래너 제목 :";
+				textArea_1.style.css({
+					"text-align" : "center"
+				});
+				container.addChild(textArea_1, {
+					"top": "0px",
+					"bottom": "0px",
+					"left": "0px",
+					"width": "185px"
+				});
+				var inputBox_1 = new cpr.controls.InputBox("ipb1");
+				container.addChild(inputBox_1, {
+					"top": "0px",
+					"right": "0px",
+					"left": "184px",
+					"height": "22px"
+				});
+				var output_1 = new cpr.controls.Output("plannerNo");
+				output_1.visible = false;
+				output_1.bind("value").toDataMap(app.lookup("plannerNoDM"), "plannerNo");
+				container.addChild(output_1, {
+					"top": "0px",
+					"left": "0px",
+					"width": "100px",
+					"height": "20px"
+				});
+			})(group_1);
+			container.addChild(group_1, {
+				"top": "20px",
+				"left": "20px",
+				"width": "848px",
+				"height": "22px"
+			});
+			
+			var group_2 = new cpr.controls.Container();
+			var formLayout_1 = new cpr.controls.layouts.FormLayout();
+			formLayout_1.scrollable = false;
+			formLayout_1.topMargin = "5px";
+			formLayout_1.rightMargin = "5px";
+			formLayout_1.bottomMargin = "5px";
+			formLayout_1.leftMargin = "5px";
+			formLayout_1.horizontalSpacing = "25px";
+			formLayout_1.verticalSpacing = "25px";
+			formLayout_1.setColumns(["50px", "50px"]);
+			formLayout_1.setRows(["25px"]);
+			group_2.setLayout(formLayout_1);
+			(function(container){
+				var button_1 = new cpr.controls.Button();
+				button_1.value = "저장";
+				container.addChild(button_1, {
+					"colIndex": 0,
+					"rowIndex": 0,
+					"verticalAlign": "top"
+				});
+				var button_2 = new cpr.controls.Button();
+				button_2.value = "취소";
+				container.addChild(button_2, {
+					"colIndex": 1,
+					"rowIndex": 0
+				});
+			})(group_2);
+			container.addChild(group_2, {
+				"top": "14px",
+				"left": "867px",
+				"width": "150px",
+				"height": "33px"
+			});
+			
+			var grid_1 = new cpr.controls.Grid("grd2");
+			grid_1.init({
+				"dataSet": app.lookup("jeju"),
+				"columns": [
+					{"width": "25px"},
+					{"width": "100px"}
+				],
+				"header": {
+					"rows": [{"height": "24px"}],
+					"cells": [
+						{
+							"constraint": {"rowIndex": 0, "colIndex": 0},
+							"configurator": function(cell){
+								cell.columnType = "checkbox";
+								cell.filterable = false;
+								cell.sortable = false;
+							}
+						},
+						{
+							"constraint": {"rowIndex": 0, "colIndex": 1},
+							"configurator": function(cell){
+								cell.filterable = false;
+								cell.sortable = false;
+								cell.targetColumnName = "title";
+								cell.text = "여행지 목록";
+							}
+						}
+					]
+				},
+				"detail": {
+					"rows": [{"height": "24px"}],
+					"cells": [
+						{
+							"constraint": {"rowIndex": 0, "colIndex": 0},
+							"configurator": function(cell){
+								cell.columnType = "checkbox";
+							}
+						},
+						{
+							"constraint": {"rowIndex": 0, "colIndex": 1},
+							"configurator": function(cell){
+								cell.columnName = "title";
+							}
+						}
+					]
+				}
+			});
+			if(typeof onGrd2Mousedown == "function") {
+				grid_1.addEventListener("mousedown", onGrd2Mousedown);
+			}
+			if(typeof onGrd2Dblclick == "function") {
+				grid_1.addEventListener("dblclick", onGrd2Dblclick);
+			}
+			container.addChild(grid_1, {
+				"top": "73px",
+				"left": "360px",
+				"width": "200px",
+				"height": "680px"
+			});
+			
+			var grid_2 = new cpr.controls.Grid("grd1");
+			grid_2.init({
+				"columns": [{"width": "137px"}],
+				"header": {
+					"rows": [{"height": "24px"}],
+					"cells": [{
+						"constraint": {"rowIndex": 0, "colIndex": 0},
+						"configurator": function(cell){
+							cell.text = "선택한 여행지 목록";
+						}
+					}]
+				},
+				"detail": {
+					"rows": [{"height": "24px"}],
+					"cells": [{
+						"constraint": {"rowIndex": 0, "colIndex": 0},
+						"configurator": function(cell){
+						}
+					}]
+				}
+			});
+			container.addChild(grid_2, {
+				"top": "73px",
+				"left": "150px",
+				"width": "200px",
+				"height": "680px"
+			});
+			
+			var group_3 = new cpr.controls.Container();
+			var xYLayout_3 = new cpr.controls.layouts.XYLayout();
+			group_3.setLayout(xYLayout_3);
+			(function(container){
+				var grid_3 = new cpr.controls.Grid("grd3");
+				grid_3.init({
+					"dataSet": app.lookup("planDate"),
+					"columns": [{"width": "100px"}],
+					"header": {
+						"rows": [{"height": "24px"}],
+						"cells": [{
+							"constraint": {"rowIndex": 0, "colIndex": 0},
+							"configurator": function(cell){
+								cell.filterable = false;
+								cell.sortable = false;
+								cell.targetColumnName = "planDate";
+								cell.text = "여행일자";
+							}
+						}]
+					},
+					"detail": {
+						"rows": [{"height": "24px"}],
+						"cells": [{
+							"constraint": {"rowIndex": 0, "colIndex": 0},
+							"configurator": function(cell){
+								cell.columnName = "planDate";
+							}
+						}]
+					}
+				});
+				container.addChild(grid_3, {
+					"top": "0px",
+					"bottom": "0px",
+					"width": "120px",
+					"left": "calc(50% - 60px)"
+				});
+			})(group_3);
+			container.addChild(group_3, {
+				"top": "73px",
+				"left": "20px",
+				"width": "120px",
+				"height": "680px"
+			});
+			
+			var textArea_2 = new cpr.controls.TextArea("txa1");
+			textArea_2.value = "제주도 지역 검색";
+			textArea_2.style.css({
+				"text-align" : "center"
+			});
+			container.addChild(textArea_2, {
+				"top": "47px",
+				"left": "20px",
+				"width": "259px",
+				"height": "20px"
+			});
+			
+			var inputBox_2 = new cpr.controls.InputBox("titleSearch");
+			var dataMapContext_1 = new cpr.bind.DataMapContext(app.lookup("areaSearch"));
+			inputBox_2.setBindContext(dataMapContext_1);
+			inputBox_2.bind("value").toDataMap(app.lookup("areaSearch"), "title");
+			if(typeof onTitleSearchKeydown == "function") {
+				inputBox_2.addEventListener("keydown", onTitleSearchKeydown);
+			}
+			container.addChild(inputBox_2, {
+				"top": "47px",
+				"left": "289px",
+				"width": "100px",
+				"height": "20px"
+			});
+			
+			var button_3 = new cpr.controls.Button("searchbtn");
+			button_3.value = "검색";
+			if(typeof onSearchbtnClick == "function") {
+				button_3.addEventListener("click", onSearchbtnClick);
+			}
+			container.addChild(button_3, {
+				"top": "47px",
+				"left": "399px",
+				"width": "100px",
+				"height": "20px"
+			});
+			
+			var embeddedPage_1 = new cpr.controls.EmbeddedPage("ep1");
+			embeddedPage_1.src = "thirdparty/maps/kakaoMapAPI.html";
+			container.addChild(embeddedPage_1, {
+				"top": "77px",
+				"left": "570px",
+				"width": "434px",
+				"height": "671px"
+			});
+			if(typeof onBodyLoad == "function"){
+				app.addEventListener("load", onBodyLoad);
+			}
 		}
 	});
 	app.title = "planner";
