d527dab89523b30a0193592c044b49886f61c4e1
hosose <hosose22@gmail.com>
2023-08-16 10:06:05 +0900

    docs - planner-board-list

--- FourGuys/src/main/resources/static/planner-board-list.clx.js
+++ FourGuys/src/main/resources/static/planner-board-list.clx.js
@@ -81,7 +81,7 @@
 			 * "글 작성" 버튼에서 click 이벤트 발생 시 호출.
 			 * 사용자가 컨트롤을 클릭할 때 발생하는 이벤트.
 			 */
-			function onButtonClick(e){
+			function onButtonClick(e) {
 				var button = e.control;
 				
 			};
