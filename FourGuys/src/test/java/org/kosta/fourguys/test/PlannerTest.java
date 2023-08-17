35925d2bc91ad70a982b249e8adcc0f610ddabda
hosose <hosose22@gmail.com>
2023-08-17 10:59:47 +0900

    docs - plannerTest.java

--- FourGuys/src/test/java/org/kosta/fourguys/test/PlannerTest.java
+++ FourGuys/src/test/java/org/kosta/fourguys/test/PlannerTest.java
@@ -97,7 +97,7 @@ public class PlannerTest {
 	@Test
 	public void findPlannerBoardByNo() {
 		int boardNo = 1;
-		PlannerBoardVO boardList = plannerBoardMapper.findPlannerBoardByNo(boardNo);
+		List<PlannerBoardVO> boardList = plannerBoardMapper.findPlannerBoardByNo(boardNo);
 		System.out.println(boardList);
 	}
 
